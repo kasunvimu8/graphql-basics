9e9a7b19275e23a2ee51fe7b60ee6eb5bdc89eb8
kasun <kasunvimu8@gmail.com>
2023-10-14 19:43:48 +0200

    Crud operations with dummy db

--- index.js
+++ index.js
@@ -1,16 +1,87 @@
 import { ApolloServer } from "@apollo/server";
-import {startStanderloneServer} from "@apollo/server/standalone";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-import {typeDefs} from "./schema.js";
+import { typeDefs } from "./schema.js";
+import data from "./_db.js";
 
-const port = "4000"
-const server = new ApolloServer({
-    typeDefs,
+const port = "4000";
 
+const resolvers = {
+  Query: {
+    games: () => {
+      return data.games;
+    },
+    authors: () => {
+      return data.authors;
+    },
+    reviews: () => {
+      return data.reviews;
+    },
+    review(_, args, context) {
+      return data.reviews.find((r) => r.id === args.id);
+    },
+    author(_, args, context) {
+      return data.authors.find((a) => a.id === args.id);
+    },
+    game(_, args, context) {
+      return data.games.find((g) => g.id === args.id);
+    },
+  },
+
+  //   Mutation resolvers Which handle update add delete mutations
+  Mutation: {
+    deleteGame(_, args, context) {
+      data.games = data.games.filter((g) => g.id !== args.id);
+      return data.games;
+    },
+    addGame(_, args, context) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      data.games.push(game);
+
+      return game;
+    },
+    updateGame(_, args) {
+      data.games = data.games.map((game) => {
+        if (game.id === args.id) {
+          return { ...game, ...args.edits };
+        }
+        return game;
+      });
+
+      return data.games.find(game => game.id === args.id);
+    },
+  },
+
+  // These are to get nested properties and their relationships ( Kinds making the forign keys and relationships)
+  Game: {
+    reviews(parents, _1, _2) {
+      return data.reviews.filter((r) => r.id === parents.id);
+    },
+  },
+
+  Author: {
+    reviews(parents) {
+      return data.reviews.filter((r) => r.id === parents.id);
+    },
+  },
+
+  Review: {
+    author(parents) {
+      return data.authors.find((a) => a.id === parents.author_id);
+    },
+    game(parents) {
+      return data.games.find((g) => g.id === parents.game_id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: port },
 });
 
-const {url} = await startStanderloneServer(server, {
-    listen: {port : port}
-})
-
-console.log("Server is listening on : "+ url +  " port : " + port);
+console.log("Server is listening on : " + url + " port : " + port);
--- schema.js
+++ schema.js
@@ -1,23 +1,49 @@
 export const typeDefs = `#graphql
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String!
+        author: Author!
+        game: Game! 
+    }
     type Game {
         id : ID!
         title: String!
         platform: [String!]!
-    }
-    type Review {
-        id: ID!
-        rating: Int!
-        content: String! 
+        reviews: [Review!]
     }
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
 
+    # Entry point of GQL
     type Query {
         games : [Game]
         reviews : [Review]
         authors : [Author]
+        review (id : ID!) : Review
+        game (id : ID!) : Game
+        author (id : ID!) : Author
+    }
+
+    # to update delete add operations in GQL
+    type Mutation {
+        deleteGame (id : ID!) : [Game]
+        addGame(game : gameInput!) : Game
+        updateGame (id : ID!, edits : editGameInput) : Game
+    }
+
+    # This is the input type for new game
+    input gameInput {
+        title : String!,
+        platform : [String!]!
+    }
+    # This is the input type for update game !!! check for the required properties
+    input editGameInput {
+        title : String,
+        platform : [String!]
     }
 `;
